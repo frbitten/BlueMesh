c61954a10ee4cc31c3f1d58a1dd51d9a33b57a93
Fernando Bittencourt <frbitten@gmail.com>
2017-12-26 19:08:07 +0000

    .

--- debugRemote.sh
+++ debugRemote.sh
@@ -4,7 +4,7 @@ echo "Update code"
 sudo git pull
 
 echo "Compiling code"
-sudo javac -d bin -sourcepath src -cp lib/bluecove-bluez-2.1.1-SNAPSHOT.jar src/bluemesh/test/TestBlueMesh.java
+sudo javac -d bin -sourcepath src -cp lib/bluecove-bluez-2.1.1-SNAPSHOT.jar;bluecove-2.1.1-SNAPSHOT.jar src/bluemesh/test/TestBlueMesh.java
 
 echo "Debug code"
 sudo java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n Main
